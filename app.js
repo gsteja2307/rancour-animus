feadff4686b3a64f2be8fe73cfa64b040ad645a6
gsteja2307 <gsuryateja05@gmail.com>
2020-03-12 22:54:52 +0530

    added handlebars template and intialised views using teh express-handlebars fw

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express"); // import express
-
+const exphbs  = require("express-handlebars") // import handlebarsjs the template frame work
 const app = express(); // initialise app
+
+// add middlewares below this
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 
 // Index Route
 app.get('/',(req, res)=>{
-    res.send("INDEX")
+    const title = "Welcome to Rancour-Animus";
+    res.render("index",{
+        title: title
+    });
 });
 
 // About Route
 app.get("/about",(req, res)=>{
-    res.send('ABOUT');
+    res.render('about');
 })
 
 const port = 5000;
