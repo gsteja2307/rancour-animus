8064c9358a86b98c84974c3b6b04efca1bf0dfa7
gsteja2307 <gsuryateja05@gmail.com>
2020-03-12 21:42:51 +0530

    removed the custom middleware

--- app.js
+++ app.js
@@ -2,22 +2,11 @@ const express = require("express"); // import express
 
 const app = express(); // initialise app
 
-// How middleware works
-app.use(function(req, res, next){
-    // console.log(Date.now());
-    req.name = "Surya Teja";
-    next();
-});
-
 // Index Route
 app.get('/',(req, res)=>{
-    console.log(req.name)
     res.send("INDEX")
 });
-// sample Route
-app.get('/surya',(req, res)=>{
-    res.send(req.name)
-});
+
 // About Route
 app.get("/about",(req, res)=>{
     res.send('ABOUT');
@@ -28,4 +17,4 @@ const port = 5000;
 app.listen(port , ()=>{
     //callback function
     console.log(`Server started on port ${port}`);
-});// listen to the port specified above
+});
