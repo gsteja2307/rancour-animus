960f33d5a32a750a01f7659c8ee26beaebee0a36
gsteja2307 <gsuryateja05@gmail.com>
2020-03-13 03:27:22 +0530

    edit idea form

--- app.js
+++ app.js
@@ -62,6 +62,17 @@ app.get("/ideas",(req, res)=>{
 app.get("/ideas/add",(req, res)=>{
     res.render('ideas/add');
 })
+// Edit Idea Form
+app.get("/ideas/edit/:id",(req, res)=>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit',{
+            idea:idea
+        });
+    })
+})
 
 // Process Form
 app.post('/ideas', (req,res) => {
