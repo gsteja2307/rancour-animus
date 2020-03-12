97be8f93012b7e9a1d8f9eb80fef468ce2c5165a
gsteja2307 <gsuryateja05@gmail.com>
2020-03-12 21:32:04 +0530

    added default(/) route and about(/about) route

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express"); // import express
 
 const app = express(); // initialise app
 
+// Index Route
+app.get('/',(req, res)=>{
+    res.send("INDEX")
+});
+
+// About Route
+app.get("/about",(req, res)=>{
+    res.send('ABOUT');
+})
+
 const port = 5000;
 
 app.listen(port , ()=>{
