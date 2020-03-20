6194fc2f6a0d807a0ecd33779b30eaf0c04db670
gsteja2307 <gsuryateja05@gmail.com>
2020-03-21 02:27:39 +0530

    made error partial and then did registration validations

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        rquired: true
+    },
+    password:{
+        type: Date,
+        default: Date.now
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('users', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,29 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 });
 
+// Register Form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if(req.body.password != req.body.password2){
+        errors.push({text:'Passwords do not match'});
+    }
+
+    if(req.body.password.length < 4){
+        errors.push({text:'Password must be atleast 5 characters long'});
+    }
+
+    if(errors.length > 0){
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2,
+        });
+    } else{
+        res.send('passed')
+    }
+});
 
 module.exports = router;
