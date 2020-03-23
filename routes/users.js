b83a9be56c8abb4dbc4456d05b6b108ead0cf7a5
gsteja2307 <gsuryateja05@gmail.com>
2020-03-24 01:50:20 +0530

    Logout functionality is implemented and is now working

--- routes/users.js
+++ routes/users.js
@@ -81,4 +81,11 @@ router.post('/register', (req, res) => {
     }
 });
 
+// Logout User
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash("success_msg", 'You are logged out')
+    res.redirect('/users/login')
+});
+
 module.exports = router;
